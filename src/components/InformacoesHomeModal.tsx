4da4a8afd20c759d764128e6a402efeca5a98380
Josué Teobaldo <josueteobaldo220@hotmail.com>
2024-06-07 18:24:51 -0300

    feat(HomePage): :sparkles: adicionando modal de mais informações
    adicionei a modal css imagens e mudei a home

---
+++ src/components/InformacoesHomeModal.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Modal, Carousel, Image } from 'antd';
+import praiaRio from '../images/praia-rj.jpg';
+import './InformacoesHomeModal.css';
+
+interface InformacoesHomeModalProps {
+  visible: boolean;
+  onClose: () => void;
+}
+
+const InformacoesHomeModal: React.FC<InformacoesHomeModalProps> = ({ visible, onClose }) => {
+  return (
+    <Modal
+      title="Como nós operamos?"
+      visible={visible}
+      onCancel={onClose}
+      footer={null}
+      className="informacoes-home-modal"
+      width="50%"
+      centered
+      closable={true}
+      closeIcon={<span>&times;</span>}
+      afterClose={() => console.log("Modal closed")}
+    >
+      <Carousel autoplay dotPosition="bottom" infinite={false}>
+        <div className="carousel-slide">
+          <Image src={praiaRio} alt="Imagem 1" className="carousel-image" />
+          <p className="carousel-description">Texto genérico abaixo da imagem 1</p>
+        </div>
+        <div className="carousel-slide">
+          <Image src={praiaRio} alt="Imagem 2" className="carousel-image" />
+          <p className="carousel-description">Texto genérico abaixo da imagem 2</p>
+        </div>
+      </Carousel>
+    </Modal>
+  );
+};
+
+export default InformacoesHomeModal;
